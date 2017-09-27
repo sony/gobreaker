e9f0a0b2b1225af7fa0d480a361093e91abc9ed7
borey <booxlp@gmail.com>
2017-09-27 16:54:08 +0800

    Update http_breaker.go
    Test results more intuitive

--- example/http_breaker.go
+++ example/http_breaker.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/sony/gobreaker"
 )
@@ -14,8 +16,10 @@ var cb *gobreaker.CircuitBreaker
 func init() {
 	var st gobreaker.Settings
 	st.Name = "HTTP GET"
+	st.Timeout = time.Second * 10
 	st.ReadyToTrip = func(counts gobreaker.Counts) bool {
 		failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
+		fmt.Printf("counts.Requests = %d, failureRatio = %v\n", counts.Requests, failureRatio)
 		return counts.Requests >= 3 && failureRatio >= 0.6
 	}
 
@@ -46,10 +50,20 @@ func Get(url string) ([]byte, error) {
 }
 
 func main() {
-	body, err := Get("http://www.google.com/robots.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	rand.Seed(time.Now().Unix())
+	for {
+		select {
+		case <-time.Tick(time.Second):
+			stringslice := []string{"http://www.google.com/robots.txt", "http://127.0.0.1/test.txt", "http://127.0.0.1/readme.txt"}
+			idx := rand.Intn(len(stringslice))
 
-	fmt.Println("%s", string(body))
+			_, err := Get(stringslice[idx])
+			if err != nil {
+				log.Println(err)
+			} else {
+				fmt.Printf("get %s success\n", stringslice[idx])
+			}
+
+		}
+	}
 }
