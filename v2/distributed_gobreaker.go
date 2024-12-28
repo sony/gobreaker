f8351f69f7c5d113c8c741d45164cc73e7c613cc
Yoshiyuki Mineo <Yoshiyuki.Mineo@jp.sony.com>
2024-12-28 22:07:56 +0900

    Title: Implement Distributed Circuit Breaker (#70) (#73)
    * Title: Implement Distributed Circuit Breaker (#70)
    
    * feature/redis-circuit-breaker
    
    * feature/redis-circuit-breaker
    
    * Refactor
    
    * save state
    
    * Saving half-open state also
    
    * Saving half-open state also
    
    * Added test case
    
    * Saving state transition
    
    * Pass context
    
    * Moved redis circuit breaker to v2
    
    * Revert go.mod and go.sum
    
    * Acked review comments
    
    * Refactor
    
    * Refactor
    
    ---------
    
    Co-authored-by: Kalpit Pant <kalpit@setu.co>
    
    * Rename
    
    * Rename
    
    * Refactor
    
    * Rename
    
    * Rename
    
    * Use generic cache store (#74)
    
    Co-authored-by: Kalpit Pant <kalpit@setu.co>
    
    * Update distributed_gobreaker.go
    
    * Update distributed_gobreaker.go
    
    * Update distributed_gobreaker.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    * Update distributed_gobreaker_test.go
    
    ---------
    
    Co-authored-by: kp <kalpitpant32@gmail.com>
    Co-authored-by: Kalpit Pant <kalpit@setu.co>

---
+++ v2/distributed_gobreaker.go
@@ -0,0 +1,265 @@
+package gobreaker
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"time"
+)
+
+var (
+	// ErrNoSharedStore is returned when there is no shared store.
+	ErrNoSharedStore = errors.New("no shared store")
+	// ErrNoSharedState is returned when there is no shared state.
+	ErrNoSharedState = errors.New("no shared state")
+)
+
+// SharedState represents the shared state of DistributedCircuitBreaker.
+type SharedState struct {
+	State      State     `json:"state"`
+	Generation uint64    `json:"generation"`
+	Counts     Counts    `json:"counts"`
+	Expiry     time.Time `json:"expiry"`
+}
+
+// SharedDataStore stores the shared state of DistributedCircuitBreaker.
+type SharedDataStore interface {
+	GetData(ctx context.Context, name string) ([]byte, error)
+	SetData(ctx context.Context, name string, data []byte) error
+}
+
+// DistributedCircuitBreaker extends CircuitBreaker with SharedDataStore.
+type DistributedCircuitBreaker[T any] struct {
+	*CircuitBreaker[T]
+	store SharedDataStore
+}
+
+// NewDistributedCircuitBreaker returns a new DistributedCircuitBreaker.
+func NewDistributedCircuitBreaker[T any](ctx context.Context, store SharedDataStore, settings Settings) (*DistributedCircuitBreaker[T], error) {
+	if store == nil {
+		return nil, ErrNoSharedStore
+	}
+
+	dcb := &DistributedCircuitBreaker[T]{
+		CircuitBreaker: NewCircuitBreaker[T](settings),
+		store:          store,
+	}
+	state := SharedState{
+		State:      dcb.state,
+		Generation: dcb.generation,
+		Counts:     dcb.counts,
+		Expiry:     dcb.expiry,
+	}
+	err := dcb.setSharedState(ctx, state)
+	return dcb, err
+}
+
+func (dcb *DistributedCircuitBreaker[T]) sharedStateKey() string {
+	return "gobreaker:" + dcb.name
+}
+
+func (dcb *DistributedCircuitBreaker[T]) getSharedState(ctx context.Context) (SharedState, error) {
+	var state SharedState
+	if dcb.store == nil {
+		return state, ErrNoSharedStore
+	}
+
+	data, err := dcb.store.GetData(ctx, dcb.sharedStateKey())
+	if len(data) == 0 {
+		return state, ErrNoSharedState
+	} else if err != nil {
+		return state, err
+	}
+
+	err = json.Unmarshal(data, &state)
+	return state, err
+}
+
+func (dcb *DistributedCircuitBreaker[T]) setSharedState(ctx context.Context, state SharedState) error {
+	if dcb.store == nil {
+		return ErrNoSharedStore
+	}
+
+	data, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+
+	return dcb.store.SetData(ctx, dcb.sharedStateKey(), data)
+}
+
+// State returns the State of DistributedCircuitBreaker.
+func (dcb *DistributedCircuitBreaker[T]) State(ctx context.Context) (State, error) {
+	state, err := dcb.getSharedState(ctx)
+	if err != nil {
+		return state.State, err
+	}
+
+	now := time.Now()
+	currentState, _ := dcb.currentState(state, now)
+
+	// update the state if it has changed
+	if currentState != state.State {
+		state.State = currentState
+		if err := dcb.setSharedState(ctx, state); err != nil {
+			return state.State, err
+		}
+	}
+
+	return state.State, nil
+}
+
+// Execute runs the given request if the DistributedCircuitBreaker accepts it.
+func (dcb *DistributedCircuitBreaker[T]) Execute(ctx context.Context, req func() (T, error)) (t T, err error) {
+	generation, err := dcb.beforeRequest(ctx)
+	if err != nil {
+		var defaultValue T
+		return defaultValue, err
+	}
+
+	defer func() {
+		e := recover()
+		if e != nil {
+			ae := dcb.afterRequest(ctx, generation, false)
+			if err == nil {
+				err = ae
+			}
+			panic(e)
+		}
+	}()
+
+	result, err := req()
+	ae := dcb.afterRequest(ctx, generation, dcb.isSuccessful(err))
+	if err == nil {
+		err = ae
+	}
+	return result, err
+}
+
+func (dcb *DistributedCircuitBreaker[T]) beforeRequest(ctx context.Context) (uint64, error) {
+	state, err := dcb.getSharedState(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	now := time.Now()
+	currentState, generation := dcb.currentState(state, now)
+
+	if currentState != state.State {
+		dcb.setState(&state, currentState, now)
+		err = dcb.setSharedState(ctx, state)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	if currentState == StateOpen {
+		return generation, ErrOpenState
+	} else if currentState == StateHalfOpen && state.Counts.Requests >= dcb.maxRequests {
+		return generation, ErrTooManyRequests
+	}
+
+	state.Counts.onRequest()
+	err = dcb.setSharedState(ctx, state)
+	if err != nil {
+		return 0, err
+	}
+
+	return generation, nil
+}
+
+func (dcb *DistributedCircuitBreaker[T]) afterRequest(ctx context.Context, before uint64, success bool) error {
+	state, err := dcb.getSharedState(ctx)
+	if err != nil {
+		return err
+	}
+
+	now := time.Now()
+	currentState, generation := dcb.currentState(state, now)
+	if generation != before {
+		return nil
+	}
+
+	if success {
+		dcb.onSuccess(&state, currentState, now)
+	} else {
+		dcb.onFailure(&state, currentState, now)
+	}
+	return dcb.setSharedState(ctx, state)
+}
+
+func (dcb *DistributedCircuitBreaker[T]) onSuccess(state *SharedState, currentState State, now time.Time) {
+	if state.State == StateOpen {
+		state.State = currentState
+	}
+
+	switch currentState {
+	case StateClosed:
+		state.Counts.onSuccess()
+	case StateHalfOpen:
+		state.Counts.onSuccess()
+		if state.Counts.ConsecutiveSuccesses >= dcb.maxRequests {
+			dcb.setState(state, StateClosed, now)
+		}
+	}
+}
+
+func (dcb *DistributedCircuitBreaker[T]) onFailure(state *SharedState, currentState State, now time.Time) {
+	switch currentState {
+	case StateClosed:
+		state.Counts.onFailure()
+		if dcb.readyToTrip(state.Counts) {
+			dcb.setState(state, StateOpen, now)
+		}
+	case StateHalfOpen:
+		dcb.setState(state, StateOpen, now)
+	}
+}
+
+func (dcb *DistributedCircuitBreaker[T]) currentState(state SharedState, now time.Time) (State, uint64) {
+	switch state.State {
+	case StateClosed:
+		if !state.Expiry.IsZero() && state.Expiry.Before(now) {
+			dcb.toNewGeneration(&state, now)
+		}
+	case StateOpen:
+		if state.Expiry.Before(now) {
+			dcb.setState(&state, StateHalfOpen, now)
+		}
+	}
+	return state.State, state.Generation
+}
+
+func (dcb *DistributedCircuitBreaker[T]) setState(state *SharedState, newState State, now time.Time) {
+	if state.State == newState {
+		return
+	}
+
+	prev := state.State
+	state.State = newState
+
+	dcb.toNewGeneration(state, now)
+
+	if dcb.onStateChange != nil {
+		dcb.onStateChange(dcb.name, prev, newState)
+	}
+}
+
+func (dcb *DistributedCircuitBreaker[T]) toNewGeneration(state *SharedState, now time.Time) {
+	state.Generation++
+	state.Counts.clear()
+
+	var zero time.Time
+	switch state.State {
+	case StateClosed:
+		if dcb.interval == 0 {
+			state.Expiry = zero
+		} else {
+			state.Expiry = now.Add(dcb.interval)
+		}
+	case StateOpen:
+		state.Expiry = now.Add(dcb.timeout)
+	default: // StateHalfOpen
+		state.Expiry = zero
+	}
+}
---
+++ v2/distributed_gobreaker_test.go
@@ -0,0 +1,345 @@
+package gobreaker
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+)
+
+type storeAdapter struct {
+	client *redis.Client
+}
+
+func (r *storeAdapter) GetData(ctx context.Context, key string) ([]byte, error) {
+	return r.client.Get(ctx, key).Bytes()
+}
+
+func (r *storeAdapter) SetData(ctx context.Context, key string, value []byte) error {
+	return r.client.Set(ctx, key, value, 0).Err()
+}
+
+var redisServer *miniredis.Miniredis
+
+func setUpDCB(ctx context.Context) *DistributedCircuitBreaker[any] {
+	var err error
+	redisServer, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr: redisServer.Addr(),
+	})
+
+	store := &storeAdapter{client: client}
+
+	dcb, err := NewDistributedCircuitBreaker[any](ctx, store, Settings{
+		Name:        "TestBreaker",
+		MaxRequests: 3,
+		Interval:    time.Second,
+		Timeout:     time.Second * 2,
+		ReadyToTrip: func(counts Counts) bool {
+			return counts.ConsecutiveFailures > 5
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return dcb
+}
+
+func tearDownDCB(dcb *DistributedCircuitBreaker[any]) {
+	if dcb != nil {
+		store := dcb.store.(*storeAdapter)
+		store.client.Close()
+		store.client = nil
+	}
+
+	if redisServer != nil {
+		redisServer.Close()
+		redisServer = nil
+	}
+}
+
+func dcbPseudoSleep(ctx context.Context, dcb *DistributedCircuitBreaker[any], period time.Duration) {
+	state, err := dcb.getSharedState(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	state.Expiry = state.Expiry.Add(-period)
+	// Reset counts if the interval has passed
+	if time.Now().After(state.Expiry) {
+		state.Counts = Counts{}
+	}
+
+	err = dcb.setSharedState(ctx, state)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func successRequest(ctx context.Context, dcb *DistributedCircuitBreaker[any]) error {
+	_, err := dcb.Execute(ctx, func() (interface{}, error) { return nil, nil })
+	return err
+}
+
+func failRequest(ctx context.Context, dcb *DistributedCircuitBreaker[any]) error {
+	_, err := dcb.Execute(ctx, func() (interface{}, error) { return nil, errors.New("fail") })
+	if err != nil && err.Error() == "fail" {
+		return nil
+	}
+	return err
+}
+
+func assertState(ctx context.Context, t *testing.T, dcb *DistributedCircuitBreaker[any], expected State) {
+	state, err := dcb.State(ctx)
+	assert.Equal(t, expected, state)
+	assert.NoError(t, err)
+}
+
+func TestDistributedCircuitBreakerInitialization(t *testing.T) {
+	ctx := context.Background()
+	dcb := setUpDCB(ctx)
+	defer tearDownDCB(dcb)
+
+	assert.Equal(t, "TestBreaker", dcb.Name())
+	assert.Equal(t, uint32(3), dcb.maxRequests)
+	assert.Equal(t, time.Second, dcb.interval)
+	assert.Equal(t, time.Second*2, dcb.timeout)
+	assert.NotNil(t, dcb.readyToTrip)
+
+	assertState(ctx, t, dcb, StateClosed)
+}
+
+func TestDistributedCircuitBreakerStateTransitions(t *testing.T) {
+	ctx := context.Background()
+	dcb := setUpDCB(ctx)
+	defer tearDownDCB(dcb)
+
+	// Check if initial state is closed
+	assertState(ctx, t, dcb, StateClosed)
+
+	// StateClosed to StateOpen
+	for i := 0; i < 6; i++ {
+		assert.NoError(t, failRequest(ctx, dcb))
+	}
+	assertState(ctx, t, dcb, StateOpen)
+
+	// Ensure requests fail when the circuit is open
+	err := failRequest(ctx, dcb)
+	assert.Equal(t, ErrOpenState, err)
+
+	// Wait for timeout so that the state will move to half-open
+	dcbPseudoSleep(ctx, dcb, dcb.timeout)
+	assertState(ctx, t, dcb, StateHalfOpen)
+
+	// StateHalfOpen to StateClosed
+	for i := 0; i < int(dcb.maxRequests); i++ {
+		assert.NoError(t, successRequest(ctx, dcb))
+	}
+	assertState(ctx, t, dcb, StateClosed)
+
+	// StateClosed to StateOpen (again)
+	for i := 0; i < 6; i++ {
+		assert.NoError(t, failRequest(ctx, dcb))
+	}
+	assertState(ctx, t, dcb, StateOpen)
+}
+
+func TestDistributedCircuitBreakerExecution(t *testing.T) {
+	ctx := context.Background()
+	dcb := setUpDCB(ctx)
+	defer tearDownDCB(dcb)
+
+	// Test successful execution
+	result, err := dcb.Execute(ctx, func() (interface{}, error) {
+		return "success", nil
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "success", result)
+
+	// Test failed execution
+	_, err = dcb.Execute(ctx, func() (interface{}, error) {
+		return nil, errors.New("test error")
+	})
+	assert.Error(t, err)
+	assert.Equal(t, "test error", err.Error())
+}
+
+func TestDistributedCircuitBreakerCounts(t *testing.T) {
+	ctx := context.Background()
+	dcb := setUpDCB(ctx)
+	defer tearDownDCB(dcb)
+
+	for i := 0; i < 5; i++ {
+		assert.Nil(t, successRequest(ctx, dcb))
+	}
+
+	state, err := dcb.getSharedState(ctx)
+	assert.Equal(t, Counts{5, 5, 0, 5, 0}, state.Counts)
+	assert.NoError(t, err)
+
+	assert.Nil(t, failRequest(ctx, dcb))
+	state, err = dcb.getSharedState(ctx)
+	assert.Equal(t, Counts{6, 5, 1, 0, 1}, state.Counts)
+	assert.NoError(t, err)
+}
+
+var customDCB *DistributedCircuitBreaker[any]
+
+func TestCustomDistributedCircuitBreaker(t *testing.T) {
+	ctx := context.Background()
+
+	mr, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	defer mr.Close()
+
+	client := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+
+	store := &storeAdapter{client: client}
+
+	customDCB, err = NewDistributedCircuitBreaker[any](ctx, store, Settings{
+		Name:        "CustomBreaker",
+		MaxRequests: 3,
+		Interval:    time.Second * 30,
+		Timeout:     time.Second * 90,
+		ReadyToTrip: func(counts Counts) bool {
+			numReqs := counts.Requests
+			failureRatio := float64(counts.TotalFailures) / float64(numReqs)
+			return numReqs >= 3 && failureRatio >= 0.6
+		},
+	})
+	assert.NoError(t, err)
+
+	t.Run("Initialization", func(t *testing.T) {
+		assert.Equal(t, "CustomBreaker", customDCB.Name())
+		assertState(ctx, t, customDCB, StateClosed)
+	})
+
+	t.Run("Counts and State Transitions", func(t *testing.T) {
+		// Perform 5 successful and 5 failed requests
+		for i := 0; i < 5; i++ {
+			assert.NoError(t, successRequest(ctx, customDCB))
+			assert.NoError(t, failRequest(ctx, customDCB))
+		}
+
+		state, err := customDCB.getSharedState(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, StateClosed, state.State)
+		assert.Equal(t, Counts{10, 5, 5, 0, 1}, state.Counts)
+
+		// Perform one more successful request
+		assert.NoError(t, successRequest(ctx, customDCB))
+		state, err = customDCB.getSharedState(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, Counts{11, 6, 5, 1, 0}, state.Counts)
+
+		// Simulate time passing to reset counts
+		dcbPseudoSleep(ctx, customDCB, time.Second*30)
+
+		// Perform requests to trigger StateOpen
+		assert.NoError(t, successRequest(ctx, customDCB))
+		assert.NoError(t, failRequest(ctx, customDCB))
+		assert.NoError(t, failRequest(ctx, customDCB))
+
+		// Check if the circuit breaker is now open
+		assertState(ctx, t, customDCB, StateOpen)
+
+		state, err = customDCB.getSharedState(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, Counts{0, 0, 0, 0, 0}, state.Counts)
+	})
+
+	t.Run("Timeout and Half-Open State", func(t *testing.T) {
+		// Simulate timeout to transition to half-open state
+		dcbPseudoSleep(ctx, customDCB, time.Second*90)
+		assertState(ctx, t, customDCB, StateHalfOpen)
+
+		// Successful requests in half-open state should close the circuit
+		for i := 0; i < 3; i++ {
+			assert.NoError(t, successRequest(ctx, customDCB))
+		}
+		assertState(ctx, t, customDCB, StateClosed)
+	})
+}
+
+func TestCustomDistributedCircuitBreakerStateTransitions(t *testing.T) {
+	// Setup
+	var stateChange StateChange
+	customSt := Settings{
+		Name:        "cb",
+		MaxRequests: 3,
+		Interval:    5 * time.Second,
+		Timeout:     5 * time.Second,
+		ReadyToTrip: func(counts Counts) bool {
+			return counts.ConsecutiveFailures >= 2
+		},
+		OnStateChange: func(name string, from State, to State) {
+			stateChange = StateChange{name, from, to}
+		},
+	}
+
+	ctx := context.Background()
+
+	mr, err := miniredis.Run()
+	if err != nil {
+		t.Fatalf("Failed to start miniredis: %v", err)
+	}
+	defer mr.Close()
+
+	client := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+
+	store := &storeAdapter{client: client}
+
+	dcb, err := NewDistributedCircuitBreaker[any](ctx, store, customSt)
+	assert.NoError(t, err)
+
+	// Test case
+	t.Run("Circuit Breaker State Transitions", func(t *testing.T) {
+		// Initial state should be Closed
+		assertState(ctx, t, dcb, StateClosed)
+
+		// Cause two consecutive failures to trip the circuit
+		for i := 0; i < 2; i++ {
+			err := failRequest(ctx, dcb)
+			assert.NoError(t, err, "Fail request should not return an error")
+		}
+
+		// Circuit should now be Open
+		assertState(ctx, t, dcb, StateOpen)
+		assert.Equal(t, StateChange{"cb", StateClosed, StateOpen}, stateChange)
+
+		// Requests should fail immediately when circuit is Open
+		err := successRequest(ctx, dcb)
+		assert.Error(t, err)
+		assert.Equal(t, ErrOpenState, err)
+
+		// Simulate timeout to transition to Half-Open
+		dcbPseudoSleep(ctx, dcb, 6*time.Second)
+		assertState(ctx, t, dcb, StateHalfOpen)
+		assert.Equal(t, StateChange{"cb", StateOpen, StateHalfOpen}, stateChange)
+
+		// Successful requests in Half-Open state should close the circuit
+		for i := 0; i < int(dcb.maxRequests); i++ {
+			err := successRequest(ctx, dcb)
+			assert.NoError(t, err)
+		}
+
+		// Circuit should now be Closed
+		assertState(ctx, t, dcb, StateClosed)
+		assert.Equal(t, StateChange{"cb", StateHalfOpen, StateClosed}, stateChange)
+	})
+}
