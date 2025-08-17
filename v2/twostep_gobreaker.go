360bbabfbc2dd4a58097a59944d7b8fc36426af7
Yoshiyuki Mineo <Yoshiyuki.Mineo@sony.com>
2025-08-17 22:18:14 +0900

    Refactor CircuitBreaker implementation by removing TwoStepCircuitBreaker
    This commit eliminates the TwoStepCircuitBreaker type and its associated methods from the gobreaker package. The changes streamline the codebase by focusing on the CircuitBreaker functionality, enhancing clarity and maintainability. Additionally, related test cases have been removed to reflect this simplification. The CircuitBreaker now handles all request management without the need for a two-step process.

---
+++ v2/twostep_gobreaker.go
@@ -0,0 +1,46 @@
+// Package gobreaker implements the Circuit Breaker pattern.
+// See https://msdn.microsoft.com/en-us/library/dn589784.aspx.
+package gobreaker
+
+// TwoStepCircuitBreaker is like CircuitBreaker but instead of surrounding a function
+// with the breaker functionality, it only checks whether a request can proceed and
+// expects the caller to report the outcome in a separate step using a callback.
+type TwoStepCircuitBreaker[T any] struct {
+	cb *CircuitBreaker[T]
+}
+
+// NewTwoStepCircuitBreaker returns a new TwoStepCircuitBreaker configured with the given Settings.
+func NewTwoStepCircuitBreaker[T any](st Settings) *TwoStepCircuitBreaker[T] {
+	return &TwoStepCircuitBreaker[T]{
+		cb: NewCircuitBreaker[T](st),
+	}
+}
+
+// Name returns the name of the TwoStepCircuitBreaker.
+func (tscb *TwoStepCircuitBreaker[T]) Name() string {
+	return tscb.cb.Name()
+}
+
+// State returns the current state of the TwoStepCircuitBreaker.
+func (tscb *TwoStepCircuitBreaker[T]) State() State {
+	return tscb.cb.State()
+}
+
+// Counts returns internal counters
+func (tscb *TwoStepCircuitBreaker[T]) Counts() Counts {
+	return tscb.cb.Counts()
+}
+
+// Allow checks if a new request can proceed. It returns a callback that should be used to
+// register the success or failure in a separate step. If the circuit breaker does not allow
+// requests, it returns an error.
+func (tscb *TwoStepCircuitBreaker[T]) Allow() (done func(success bool), err error) {
+	generation, age, err := tscb.cb.beforeRequest()
+	if err != nil {
+		return nil, err
+	}
+
+	return func(success bool) {
+		tscb.cb.afterRequest(generation, age, success)
+	}, nil
+}
